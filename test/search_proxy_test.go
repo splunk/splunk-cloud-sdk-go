9e11836b908b444fcbd3500a48ea8ec5a0b41c1b
Shilpa Bijam <sbijam@splunk.com>
2018-04-06 16:30:19 -0700

    stubby tests

---
+++ test/search_proxy_test.go
@@ -0,0 +1,71 @@
+package tests
+
+import (
+	"github.com/splunk/ssc-client-go/lib/service"
+	"time"
+	"testing"
+	"github.com/splunk/ssc-client-go/lib/model"
+)
+
+
+const (
+	ClientId = "admin"
+	ClientSecret = "changeme"
+	BaseURL = "localhost:32769"
+)
+
+func TestNewSearchJobWithStubby(t *testing.T) {
+	client := service.NewSplunkdClient(
+		"", [2]string{ClientId, ClientSecret}, BaseURL, "http", service.NewSplunkdHTTPClient(time.Second*5, true))
+
+	response, _ := client.SearchService.CreateJob("search index=*")
+
+	if response != "SEARCH_ID" {
+		t.Errorf("Expected SEARCHID not found in response")
+	}
+}
+
+func TestGetJobResultsWithStubby(t *testing.T) {
+	client := service.NewSplunkdClient(
+		"", [2]string{ClientId, ClientSecret}, BaseURL, "http", service.NewSplunkdHTTPClient(time.Second*5, true))
+
+	client.SearchService.CreateJob("search index=*")
+	response, _ := client.SearchService.GetResults("SEARCH_ID")
+
+	ValidateResponse(response, t)
+}
+
+func TestNewSearchJobSyncWithStubby(t *testing.T) {
+	client := service.NewSplunkdClient(
+		"", [2]string{ClientId, ClientSecret}, BaseURL, "http", service.NewSplunkdHTTPClient(time.Second*5, true))
+
+	response, _ := client.SearchService.CreateSyncJob("search index=*")
+
+	ValidateResponse(response, t)
+}
+
+//Validate response results
+func ValidateResponse(response *model.SearchEvents, t *testing.T) {
+
+	var indexFound bool
+
+	if response.Fields != nil {
+		for _, v := range response.Fields {
+			for m, n := range v {
+				if m == "name" && n == "index" {
+					indexFound = true
+				}
+			}
+		}
+		if !indexFound {
+			t.Errorf("Expected results field element name and corresponding value index not found")
+		}
+	} else {
+		t.Errorf("Expected field elements in results not found")
+	}
+
+	if response.Results == nil {
+		t.Errorf("Invalid response, missing results in response returned")
+	}
+
+}
