6fc231ea57945bbfbd2491e09359c6fc7f1afa0d
Dan Nguyen <dnguyen@splunk.com>
2018-05-14 14:54:51 -0700

    APPLAT-870 Increase coverage of search integration tests
    1. Test the following API calls with different params
    
    - createJob
    - createJobSync
    - getResults
    
    2. Test 400, 500 HTTP error codes where available

--- service/search_integration_test.go
+++ service/search_integration_test.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/splunk/ssc-client-go/model"
+	"github.com/splunk/ssc-client-go/util"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -15,39 +16,243 @@ var hostID = os.Getenv("SSC_HOST")
 var token = os.Getenv("BEARER_TOKEN")
 var tenantID = os.Getenv("TENANT_ID")
 
+var (
+	PostJobsRequest                        = &model.PostJobsRequest{Query: "search index=_internal | head 5"}
+	PostJobsRequestBadRequest              = &model.PostJobsRequest{}
+	PostJobsRequestBadQuery                = &model.PostJobsRequest{Query: "index=_internal | head 5"}
+	PostJobsRequestTimeout                 = &model.PostJobsRequest{Query: "search index=_internal | head 5", Timeout: 5}
+	PostJobsRequestTTL                     = &model.PostJobsRequest{Query: "search index=_internal | head 5", TTL: 5}
+	PostJobsRequestLimit                   = &model.PostJobsRequest{Query: "search index=_internal | head 5", Limit: 10}
+	PostJobsRequestDisableAutoFinalization = &model.PostJobsRequest{Query: "search index=_internal | head 5", Limit: 0}
+	PostJobsRequestMultiArgs               = &model.PostJobsRequest{Query: "search index=_internal | head 5", Timeout: 5, TTL: 10, Limit: 10}
+)
+
 func getSplunkClientForPlaygroundTests() *Client {
 	return NewClient(tenantID, token, hostID, time.Second*5)
 }
 
+// TestIntegrationEnvironment for token and tenant
 func TestIntegrationEnvironment(t *testing.T) {
 	assert.NotEmpty(t, token)
 	assert.NotEmpty(t, tenantID)
 }
 
+// TestIntegrationNewSearchJob asynchronously
 func TestIntegrationNewSearchJob(t *testing.T) {
 	client := getSplunkClientForPlaygroundTests()
 	assert.NotNil(t, client)
 
-	response, err := client.SearchService.CreateJob(&model.PostJobsRequest{Query: "search index=_internal | head 5"})
+	response, err := client.SearchService.CreateJob(PostJobsRequest)
 	assert.Nil(t, err)
 	assert.NotEmpty(t, response.SearchID)
 }
 
+// TestIntegrationNewSearchJobBadRequest asynchronously
+func TestIntegrationNewSearchJobBadRequest(t *testing.T) {
+	client := getSplunkClientForPlaygroundTests()
+	assert.NotNil(t, client)
+
+	response, err := client.SearchService.CreateJob(PostJobsRequestBadRequest)
+
+	// HTTP 400 Error Code
+	expectedError := &util.HTTPError{Status: 400, Message: "400 Bad Request"}
+
+	assert.NotNil(t, err)
+	assert.Equal(t, expectedError, err)
+	assert.Empty(t, response.SearchID)
+}
+
+// TestIntegrationNewSearchJobBadQuery asynchronously
+func TestIntegrationNewSearchJobBadQuery(t *testing.T) {
+	client := getSplunkClientForPlaygroundTests()
+	assert.NotNil(t, client)
+
+	response, err := client.SearchService.CreateJob(PostJobsRequestBadQuery)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, response.SearchID)
+}
+
+// TestIntegrationNewSearchJobDuplicates
+func TestIntegrationNewSearchJobDuplicates(t *testing.T) {
+	client := getSplunkClientForPlaygroundTests()
+	assert.NotNil(t, client)
+
+	response, err := client.SearchService.CreateJob(PostJobsRequest)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, response.SearchID)
+
+	response, err = client.SearchService.CreateJob(PostJobsRequest)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, response.SearchID)
+}
+
+// TestIntegrationNewSearchJobTimeout with timeout at 5 sec
+func TestIntegrationNewSearchJobTimeout(t *testing.T) {
+	client := getSplunkClientForPlaygroundTests()
+	assert.NotNil(t, client)
+
+	response, err := client.SearchService.CreateJob(PostJobsRequestTimeout)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, response.SearchID)
+}
+
+// TestIntegrationNewSearchJobTTL with TTL at 5 sec
+func TestIntegrationNewSearchJobTTL(t *testing.T) {
+	client := getSplunkClientForPlaygroundTests()
+	assert.NotNil(t, client)
+
+	response, err := client.SearchService.CreateJob(PostJobsRequestTTL)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, response.SearchID)
+}
+
+// TestIntegrationNewSearchJobLimit with Limit at 10
+func TestIntegrationNewSearchJobLimit(t *testing.T) {
+	client := getSplunkClientForPlaygroundTests()
+	assert.NotNil(t, client)
+
+	response, err := client.SearchService.CreateJob(PostJobsRequestLimit)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, response.SearchID)
+}
+
+// TestIntegrationNewSearchJobDisableAutoFinalization with Limit at 0, disable automatic finalization
+func TestIntegrationNewSearchJobDisableAutoFinalization(t *testing.T) {
+	client := getSplunkClientForPlaygroundTests()
+	assert.NotNil(t, client)
+
+	response, err := client.SearchService.CreateJob(PostJobsRequestDisableAutoFinalization)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, response.SearchID)
+}
+
+// TestIntegrationNewSearchJobMultiArgs with multiple args
+func TestIntegrationNewSearchJobMultiArgs(t *testing.T) {
+	client := getSplunkClientForPlaygroundTests()
+	assert.NotNil(t, client)
+
+	response, err := client.SearchService.CreateJob(PostJobsRequestMultiArgs)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, response.SearchID)
+}
+
+// TestIntegrationNewSearchJobSync
 func TestIntegrationNewSearchJobSync(t *testing.T) {
 	client := getSplunkClientForPlaygroundTests()
 	assert.NotNil(t, client)
 
-	response, err := client.SearchService.CreateSyncJob(&model.PostJobsRequest{Query: "search index=_internal | head 5"})
+	response, err := client.SearchService.CreateSyncJob(PostJobsRequest)
 	assert.Nil(t, err)
 	assert.NotNil(t, response)
 	ValidateResponses(response, t)
 }
 
+// TestIntegrationNewSearchJobBadRequest asynchronously
+func TestIntegrationNewSearchJobSyncBadRequest(t *testing.T) {
+	client := getSplunkClientForPlaygroundTests()
+	assert.NotNil(t, client)
+
+	response, err := client.SearchService.CreateSyncJob(PostJobsRequestBadRequest)
+
+	// HTTP 400 Error Code
+	expectedError := &util.HTTPError{Status: 400, Message: "400 Bad Request"}
+	assert.NotNil(t, err)
+	assert.Equal(t, expectedError, err)
+
+	// expected Search Event
+	expectedResult := &model.SearchEvents{Preview: false, InitOffset: 0, Messages: []interface{}(nil),
+		Results: []*model.Result(nil), Fields: []map[string]interface{}(nil), Highlighted: map[string]interface{}(nil)}
+	assert.NotNil(t, response)
+	assert.EqualValues(t, expectedResult, response)
+}
+
+// TestIntegrationNewSearchJobBadQuery asynchronously
+func TestIntegrationNewSearchJobSyncBadQuery(t *testing.T) {
+	client := getSplunkClientForPlaygroundTests()
+	assert.NotNil(t, client)
+
+	response, err := client.SearchService.CreateJob(PostJobsRequestBadQuery)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, response.SearchID)
+}
+
+// TestIntegrationNewSearchJobSyncDuplicates
+func TestIntegrationNewSearchJobSyncDuplicates(t *testing.T) {
+	client := getSplunkClientForPlaygroundTests()
+	assert.NotNil(t, client)
+
+	response, err := client.SearchService.CreateSyncJob(PostJobsRequest)
+	assert.Nil(t, err)
+	assert.NotNil(t, response)
+	ValidateResponses(response, t)
+
+	response, err = client.SearchService.CreateSyncJob(PostJobsRequest)
+	assert.Nil(t, err)
+	assert.NotNil(t, response)
+	ValidateResponses(response, t)
+}
+
+// TestIntegrationNewSearchJobSyncTimeout with timeout at 5 sec
+func TestIntegrationNewSearchJobSyncTimeout(t *testing.T) {
+	client := getSplunkClientForPlaygroundTests()
+	assert.NotNil(t, client)
+
+	response, err := client.SearchService.CreateSyncJob(PostJobsRequestTimeout)
+	assert.Nil(t, err)
+	assert.NotNil(t, response)
+	ValidateResponses(response, t)
+}
+
+// TestIntegrationNewSearchJobSyncTTL with TTL at 5 sec
+func TestIntegrationNewSearchJobSyncTTL(t *testing.T) {
+	client := getSplunkClientForPlaygroundTests()
+	assert.NotNil(t, client)
+
+	response, err := client.SearchService.CreateSyncJob(PostJobsRequestTTL)
+	assert.Nil(t, err)
+	assert.NotNil(t, response)
+	ValidateResponses(response, t)
+}
+
+// TestIntegrationNewSearchJobSyncLimit with Limit at 10
+func TestIntegrationNewSearchJobSyncLimit(t *testing.T) {
+	client := getSplunkClientForPlaygroundTests()
+	assert.NotNil(t, client)
+
+	response, err := client.SearchService.CreateSyncJob(PostJobsRequestLimit)
+	assert.Nil(t, err)
+	assert.NotNil(t, response)
+	ValidateResponses(response, t)
+}
+
+// TestIntegrationNewSearchJobSyncDisableAutoFinalization with Limit at 0, disable automatic finalization
+func TestIntegrationNewSearchJobSyncDisableAutoFinalization(t *testing.T) {
+	client := getSplunkClientForPlaygroundTests()
+	assert.NotNil(t, client)
+
+	response, err := client.SearchService.CreateSyncJob(PostJobsRequestDisableAutoFinalization)
+	assert.Nil(t, err)
+	assert.NotNil(t, response)
+	ValidateResponses(response, t)
+}
+
+// TestIntegrationNewSearchJobSyncMultiArgs with multiple args
+func TestIntegrationNewSearchJobSyncMultiArgs(t *testing.T) {
+	client := getSplunkClientForPlaygroundTests()
+	assert.NotNil(t, client)
+
+	response, err := client.SearchService.CreateSyncJob(PostJobsRequestMultiArgs)
+	assert.Nil(t, err)
+	assert.NotNil(t, response)
+	ValidateResponses(response, t)
+}
+
+// TestIntegrationGetJobResults
 func TestIntegrationGetJobResults(t *testing.T) {
 	client := getSplunkClientForPlaygroundTests()
 	assert.NotNil(t, client)
 
-	response, err := client.SearchService.CreateJob(&model.PostJobsRequest{Query: "search index=_internal | head 5"})
+	response, err := client.SearchService.CreateJob(PostJobsRequest)
 	assert.Nil(t, err)
 	assert.NotNil(t, response)
 
@@ -57,6 +262,26 @@ func TestIntegrationGetJobResults(t *testing.T) {
 	assert.Nil(t, err)
 	assert.NotNil(t, resp)
 	ValidateResponses(resp, t)
+}
+
+// TestIntegrationGetJobResultsBadSearchID
+func TestIntegrationGetJobResultsBadSearchID(t *testing.T) {
+	client := getSplunkClientForPlaygroundTests()
+	assert.NotNil(t, client)
+
+	// HTTP Code 500 Error
+	expectedError := &util.HTTPError{Status: 500, Message: "500 Internal Server Error"}
+
+	resp, err := client.SearchService.GetResults("NON_EXISTING_SEARCH_ID")
+	assert.NotNil(t, err)
+	assert.Equal(t, expectedError, err)
+
+	// empty SearchEvent
+	expectedSearchEvent := &model.SearchEvents{Preview: false, InitOffset: 0, Messages: []interface{}(nil),
+		Results: []*model.Result(nil), Fields: []map[string]interface{}(nil), Highlighted: map[string]interface{}(nil)}
+
+	assert.NotNil(t, resp)
+	assert.EqualValues(t, expectedSearchEvent, resp)
 }
 
 // Validate response results
