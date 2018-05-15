872b7cbcd102294f4b1e85a0c7a6d1feb336332e
Hugbot <loganknecht@users.noreply.github.com>
2018-05-15 16:10:30 -0700

    Add hec integration tests.

---
+++ service/hec_integration_test.go
@@ -0,0 +1,55 @@
+// +build !integration
+
+package service
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/splunk/ssc-client-go/model"
+)
+
+func TestIntegrationCreateEventSuccess(t *testing.T) {
+	timeValue := float64(1523637597)
+	client := getSplunkClientForPlaygroundTests()
+	testHecEvent := model.HecEvent{Host: client.URL.RequestURI(),
+		Index:      "main",
+		Event:      "test",
+		Sourcetype: "sourcetype:eventgen",
+		Source:     "manual-events",
+		Time:       &timeValue,
+		Fields:     map[string]string{"testKey": "testValue"}}
+
+	err := client.HecService.CreateEvent(testHecEvent)
+	assert.Empty(t, err)
+}
+
+func TestIntegrationCreateRawEventSuccess(t *testing.T) {
+	client := getSplunkClientForPlaygroundTests()
+	testHecEvent := model.HecEvent{Event: "test"}
+
+	err := client.HecService.CreateRawEvent(testHecEvent)
+	assert.Empty(t, err)
+}
+
+func TestIntegrationCreateEvents(t *testing.T) {
+	client := getSplunkClientForPlaygroundTests()
+	event1 := model.HecEvent{Host: "host1", Event: "test1"}
+	event2 := model.HecEvent{Host: "host2", Event: "test2"}
+	err := client.HecService.CreateEvents([]model.HecEvent{event1, event2})
+	assert.Empty(t, err)
+}
+
+func TestIntegrationBuildMultiEventsPayload(t *testing.T) {
+	client := getSplunkClientForPlaygroundTests()
+	event1 := model.HecEvent{Host: "host1", Event: "test1"}
+	event2 := model.HecEvent{Host: "host2", Event: "test2"}
+	event3WithEmptyFields := model.HecEvent{Host: "", Event: "test3"}
+	payload1, err := client.HecService.buildMultiEventsPayload([]model.HecEvent{event1, event2})
+	assert.Nil(t, err)
+	assert.Equal(t, `{"host":"host1","event":"test1"}{"host":"host2","event":"test2"}`, string(payload1[:]))
+	payload2, err := client.HecService.buildMultiEventsPayload([]model.HecEvent{event1, event3WithEmptyFields})
+	assert.Nil(t, err)
+	assert.Equal(t, `{"host":"host1","event":"test1"}{"event":"test3"}`, string(payload2[:]))
+}
