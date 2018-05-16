457e33db3bc2483cafcf7b760a907946ababfc22
Tim Pavlik <tpavlik@splunk.com>
2018-05-16 13:08:28 -0700

    Flag unit and integration tests separately for codecov.

--- ci/integration/runtests.sh
+++ ci/integration/runtests.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-source ./ci/integration/okta.sh
-cd service # TODO: this shouldn't be necessary...
+FULL_PATH=$(cd "$(dirname "$0")"; pwd)
+
+source $FULL_PATH/okta.sh
 if [ "$allow_failures" -eq "1" ]; then
     echo "Running integration tests but not gating on failures..."
     set +e
-    go test -v -run ^TestIntegration*
-    exit 0
+    go test -v -covermode=count -coverprofile="codecov.integration.out" -run ^TestIntegration* ./...
 else
     echo "Running integration tests and gating on failures..."
-    go test -v -run ^TestIntegration* || exit 1
+    go test -v -covermode=count -coverprofile="codecov.integration.out" -run ^TestIntegration* ./... || exit 1
 fi
+$FULL_PATH/../codecov -f "codecov.integration.out" -F integration
