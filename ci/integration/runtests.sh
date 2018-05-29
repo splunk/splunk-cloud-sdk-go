654b8c3f815280b0baf032bc3188e699037bd973
Shakeel Mohamed <smohamed@splunk.com>
2018-05-29 12:53:20 -0700

    Make theoretical changes for CI to consume token-fetcher

--- ci/integration/runtests.sh
+++ ci/integration/runtests.sh
@@ -8,12 +8,21 @@ env | grep TEST_URL_PROTOCOL
 env | grep TEST_TENANT_ID
 
 # Get the BEARER_TOKEN setup
-CONFIG_FILE="./.config/okta/token"
-if [ -f $CONFIG_FILE ]; then
-    echo "Token found in $CONFIG_FILE"
-    TEST_BEARER_TOKEN=$(cat $CONFIG_FILE)
+TOKEN_FILE="./.config/okta/token"
+if [ -f $TOKEN_FILE ]; then
+    echo "Token found in $TOKEN_FILE"
+    TEST_BEARER_TOKEN=$(cat $TOKEN_FILE)
 else
-    echo "Token was not set to $CONFIG_FILE"
+    echo "Token was not set to $TOKEN_FILE"
+    exit 1
+fi
+
+TENANT_FILE="./.config/ssc/tenant_id"
+if [ -f $TENANT_FILE ]; then
+    echo "Tenant found in $TENANT_FILE"
+    TEST_TENANT_ID=$(cat $TENANT_FILE)
+else
+    echo "Tenant was not set to $TENANT_FILE"
     exit 1
 fi
 
