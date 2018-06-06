d7897788b1fab26639ba8e23b0b7f906ff1ffd55
Parul Upadhyaya <pupadhyaya@splunk.com>
2018-06-06 15:52:53 -0700

    Review comments

--- test/playground_integration/catalog_integration_test.go
+++ test/playground_integration/catalog_integration_test.go
@@ -35,6 +35,8 @@ func cleanupRules(t *testing.T) {
 
 // Test CreateDataset
 func TestIntegrationCreateDataset(t *testing.T) {
+	defer cleanupDatasets(t)
+
 	client := getClient(t)
 
 	// create dataset
@@ -52,13 +54,12 @@ func TestIntegrationCreateDataset(t *testing.T) {
 	_, err = client.CatalogService.CreateDataset(
 		model.DatasetInfo{Name: "integ_dataset_3000", Kind: model.LOOKUP, Owner: datasetOwner, Capabilities: datasetCapabilities, ExternalKind: "kvcollection", ExternalName: "test_externalName"})
 	assert.Nil(t, err)
-
-	// clean up test datasets
-	cleanupDatasets(t)
 }
 
 // Test CreateDataset for 409 DatasetInfo already present error
 func TestIntegrationCreateDatasetDataAlreadyPresentError(t *testing.T) {
+	defer cleanupDatasets(t)
+
 	client := getClient(t)
 
 	// create dataset
@@ -71,13 +72,12 @@ func TestIntegrationCreateDatasetDataAlreadyPresentError(t *testing.T) {
 		model.DatasetInfo{ID: dataset.ID, Name: "integ_dataset_1000", Kind: model.LOOKUP, Owner: datasetOwner, Capabilities: datasetCapabilities, ExternalKind: "kvcollection", ExternalName: "test_externalName"})
 	assert.NotNil(t, err)
 	assert.True(t, strings.Contains(err.Error(), "409"))
-
-	// clean up test datasets
-	cleanupDatasets(t)
 }
 
 // Test CreateDataset for 401 Unauthorized operation error
 func TestIntegrationCreateDatasetUnauthorizedOperationError(t *testing.T) {
+	defer cleanupDatasets(t)
+
 	invalidClient := getInvalidClient(t)
 
 	datasetName := "integ_dataset_1000"
@@ -87,26 +87,24 @@ func TestIntegrationCreateDatasetUnauthorizedOperationError(t *testing.T) {
 		model.DatasetInfo{Name: datasetName, Kind: model.LOOKUP, Owner: datasetOwner, Capabilities: datasetCapabilities, ExternalKind: "kvcollection", ExternalName: "test_externalName"})
 	assert.NotNil(t, err)
 	assert.True(t, strings.Contains(err.Error(), "401 Unauthorized"))
-
-	// clean up test datasets
-	cleanupDatasets(t)
 }
 
 // Test CreateDataset for 400 Invalid DatasetInfo error
 func TestIntegrationCreateDatasetInvalidDatasetInfoError(t *testing.T) {
+	defer cleanupDatasets(t)
+
 	client := getClient(t)
 
 	_, err := client.CatalogService.CreateDataset(
 		model.DatasetInfo{Name: "integ_dataset_4000", Kind: model.LOOKUP})
 	assert.NotNil(t, err)
 	assert.True(t, strings.Contains(err.Error(), "400"))
-
-	// clean up test datasets
-	cleanupDatasets(t)
 }
 
 // Test GetDatasets
 func TestIntegrationGetAllDatasets(t *testing.T) {
+	defer cleanupDatasets(t)
+
 	client := getClient(t)
 
 	// create dataset
@@ -119,13 +117,12 @@ func TestIntegrationGetAllDatasets(t *testing.T) {
 	datasets, err := client.CatalogService.GetDatasets()
 	assert.Nil(t, err)
 	assert.NotNil(t, len(datasets))
-
-	// clean up test datasets
-	cleanupDatasets(t)
 }
 
 // Test GetDatasets for 401 Unauthorized operation error
 func TestIntegrationGetAllDatasetsUnauthorizedOperationError(t *testing.T) {
+	defer cleanupDatasets(t)
+
 	client := getClient(t)
 	invalidClient := getInvalidClient(t)
 
@@ -139,13 +136,12 @@ func TestIntegrationGetAllDatasetsUnauthorizedOperationError(t *testing.T) {
 	_, err = invalidClient.CatalogService.GetDatasets()
 	assert.NotNil(t, err)
 	assert.True(t, strings.Contains(err.Error(), "401 Unauthorized"))
-
-	// clean up test datasets
-	cleanupDatasets(t)
 }
 
 // Test GetDataset by ID
 func TestIntegrationGetDatasetByID(t *testing.T) {
+	defer cleanupDatasets(t)
+
 	client := getClient(t)
 
 	// create dataset
@@ -156,13 +152,12 @@ func TestIntegrationGetDatasetByID(t *testing.T) {
 	datasetByID, err := client.CatalogService.GetDataset(dataset.ID)
 	assert.Nil(t, err)
 	assert.NotNil(t, datasetByID)
-
-	// clean up test datasets
-	cleanupDatasets(t)
 }
 
 // Test GetDataset for 401 Unauthorized operation error
 func TestIntegrationGetDatasetByIDUnauthorizedOperationError(t *testing.T) {
+	defer cleanupDatasets(t)
+
 	client := getClient(t)
 	invalidClient := getInvalidClient(t)
 
@@ -174,25 +169,23 @@ func TestIntegrationGetDatasetByIDUnauthorizedOperationError(t *testing.T) {
 	_, err = invalidClient.CatalogService.GetDataset(dataset.ID)
 	assert.NotNil(t, err)
 	assert.True(t, strings.Contains(err.Error(), "401 Unauthorized"))
-
-	// clean up test datasets
-	cleanupDatasets(t)
 }
 
 // Test GetDataset for 404 DatasetInfo not found error
 func TestIntegrationGetDatasetByIDDatasetNotFoundError(t *testing.T) {
+	defer cleanupDatasets(t)
+
 	client := getClient(t)
 
 	_, err := client.CatalogService.GetDataset("123")
 	assert.NotNil(t, err)
 	assert.True(t, strings.Contains(err.Error(), "404"))
-
-	// clean up test datasets
-	cleanupDatasets(t)
 }
 
 // Test UpdateDataset
 func TestIntegrationUpdateExistingDataset(t *testing.T) {
+	defer cleanupDatasets(t)
+
 	client := getClient(t)
 
 	// create dataset
@@ -203,13 +196,12 @@ func TestIntegrationUpdateExistingDataset(t *testing.T) {
 	updatedDataset, err := client.CatalogService.UpdateDataset(model.PartialDatasetInfo{Version: 6}, dataset.ID)
 	assert.Nil(t, err)
 	assert.NotNil(t, updatedDataset)
-
-	// clean up test datasets
-	cleanupDatasets(t)
 }
 
 // Test UpdateDataset for 404 DatasetInfo not found error
 func TestIntegrationUpdateExistingDatasetDataNotFoundError(t *testing.T) {
+	defer cleanupDatasets(t)
+
 	client := getClient(t)
 
 	datasetOwner := "Splunk"
@@ -218,13 +210,12 @@ func TestIntegrationUpdateExistingDatasetDataNotFoundError(t *testing.T) {
 	_, err := client.CatalogService.UpdateDataset(model.PartialDatasetInfo{Name: "goSdkDataset6", Kind: model.LOOKUP, Owner: datasetOwner, Capabilities: datasetCapabilities, ExternalKind: "kvcollection", ExternalName: "test_externalName", Version: 2}, "123")
 	assert.NotNil(t, err)
 	assert.True(t, strings.Contains(err.Error(), "404"))
-
-	// clean up test datasets
-	cleanupDatasets(t)
 }
 
 // Test DeleteDataset
 func TestIntegrationDeleteDataset(t *testing.T) {
+	defer cleanupDatasets(t)
+
 	client := getClient(t)
 
 	// create dataset
@@ -239,13 +230,12 @@ func TestIntegrationDeleteDataset(t *testing.T) {
 	_, err = client.CatalogService.GetDataset(dataset.ID)
 	assert.NotNil(t, err)
 	assert.True(t, strings.Contains(err.Error(), "404"))
-
-	// clean up test datasets
-	cleanupDatasets(t)
 }
 
 // Test DeleteDataset for 401 Unauthorized operation error
 func TestIntegrationDeleteDatasetUnauthorizedOperationError(t *testing.T) {
+	defer cleanupDatasets(t)
+
 	client := getClient(t)
 	invalidClient := getInvalidClient(t)
 
@@ -258,27 +248,25 @@ func TestIntegrationDeleteDatasetUnauthorizedOperationError(t *testing.T) {
 	err = invalidClient.CatalogService.DeleteDataset(dataset.ID)
 	assert.NotNil(t, err)
 	assert.True(t, strings.Contains(err.Error(), "401 Unauthorized"))
-
-	// clean up test datasets
-	cleanupDatasets(t)
 }
 
 // Test DeleteDataset for 404 DatasetInfo not found error
 func TestIntegrationDeleteDatasetDataNotFoundError(t *testing.T) {
+	defer cleanupDatasets(t)
+
 	client := getClient(t)
 
 	err := client.CatalogService.DeleteDataset("123")
 	assert.NotNil(t, err)
 	assert.True(t, strings.Contains(err.Error(), "404"))
-
-	// clean up test datasets
-	cleanupDatasets(t)
 }
 
 // todo (Parul): 405 DatasetInfo cannot be deleted because of dependencies error case
 
 // Test CreateRules
 func TestIntegrationCreateRules(t *testing.T) {
+	defer cleanupRules(t)
+
 	client := getClient(t)
 
 	// create rule
@@ -299,13 +287,12 @@ func TestIntegrationCreateRules(t *testing.T) {
 	_, err = client.CatalogService.CreateRule(
 		model.Rule{Name: "thirdone", Module: ruleModule, Owner: owner})
 	assert.Nil(t, err)
-
-	// clean up test rules
-	cleanupRules(t)
 }
 
 // Test CreateRule for 409 Rule already present error
 func TestIntegrationCreateRuleDataAlreadyPresent(t *testing.T) {
+	defer cleanupRules(t)
+
 	client := getClient(t)
 
 	// create rule
@@ -322,13 +309,12 @@ func TestIntegrationCreateRuleDataAlreadyPresent(t *testing.T) {
 	_, err = client.CatalogService.CreateRule(model.Rule{ID: rule.ID, Name: ruleName, Module: ruleModule, Owner: owner})
 	assert.NotNil(t, err)
 	assert.True(t, strings.Contains(err.Error(), "409"))
-
-	// clean up test rules
-	cleanupRules(t)
 }
 
 // Test CreateRule for 401 Unauthorized operation error
 func TestIntegrationCreateRuleUnauthorizedOperationError(t *testing.T) {
+	defer cleanupRules(t)
+
 	invalidClient := getInvalidClient(t)
 
 	// create rule
@@ -339,13 +325,12 @@ func TestIntegrationCreateRuleUnauthorizedOperationError(t *testing.T) {
 	_, err := invalidClient.CatalogService.CreateRule(model.Rule{Name: ruleName, Module: ruleModule, Owner: owner})
 	assert.NotNil(t, err)
 	assert.True(t, strings.Contains(err.Error(), "401 Unauthorized"))
-
-	// clean up test rules
-	cleanupRules(t)
 }
 
 // Test GetRules
 func TestIntegrationGetAllRules(t *testing.T) {
+	defer cleanupRules(t)
+
 	client := getClient(t)
 
 	// create rule
@@ -360,13 +345,12 @@ func TestIntegrationGetAllRules(t *testing.T) {
 	rules, err := client.CatalogService.GetRules()
 	assert.Nil(t, err)
 	assert.NotNil(t, len(rules))
-
-	// clean up test rules
-	cleanupRules(t)
 }
 
 // Test GetRules for 401 Unauthorized operation error
 func TestIntegrationGetAllRulesUnauthorizedOperationError(t *testing.T) {
+	defer cleanupRules(t)
+
 	client := getClient(t)
 	invalidClient := getInvalidClient(t)
 
@@ -382,13 +366,12 @@ func TestIntegrationGetAllRulesUnauthorizedOperationError(t *testing.T) {
 	_, err = invalidClient.CatalogService.GetRules()
 	assert.NotNil(t, err)
 	assert.True(t, strings.Contains(err.Error(), "401 Unauthorized"))
-
-	// clean up test rules
-	cleanupRules(t)
 }
 
 // Test GetRule By ID
 func TestIntegrationGetRuleByID(t *testing.T) {
+	defer cleanupRules(t)
+
 	client := getClient(t)
 
 	// create rule
@@ -402,13 +385,12 @@ func TestIntegrationGetRuleByID(t *testing.T) {
 	ruleByID, err := client.CatalogService.GetRule(rule.ID)
 	assert.Nil(t, err)
 	assert.NotNil(t, ruleByID)
-
-	// clean up test rules
-	cleanupRules(t)
 }
 
 // Test GetRules for 401 Unauthorized operation error
 func TestIntegrationGetRuleByIDUnauthorizedOperationError(t *testing.T) {
+	defer cleanupRules(t)
+
 	client := getClient(t)
 	invalidClient := getInvalidClient(t)
 
@@ -423,25 +405,23 @@ func TestIntegrationGetRuleByIDUnauthorizedOperationError(t *testing.T) {
 	_, err = invalidClient.CatalogService.GetRule(rule.ID)
 	assert.NotNil(t, err)
 	assert.True(t, strings.Contains(err.Error(), "401 Unauthorized"))
-
-	// clean up test rules
-	cleanupRules(t)
 }
 
 // Test GetRules for 404 Rule not found error
 func TestIntegrationGetRuleByIDRuleNotFoundError(t *testing.T) {
+	defer cleanupRules(t)
+
 	client := getClient(t)
 
 	_, err := client.CatalogService.GetRule("123")
 	assert.NotNil(t, err)
 	assert.True(t, strings.Contains(err.Error(), "404"))
-
-	// clean up test rules
-	cleanupRules(t)
 }
 
 // Test DeleteRule by ID
 func TestIntegrationDeleteRule(t *testing.T) {
+	defer cleanupRules(t)
+
 	client := getClient(t)
 
 	// create rule
@@ -454,13 +434,12 @@ func TestIntegrationDeleteRule(t *testing.T) {
 
 	err = client.CatalogService.DeleteRule(rule.ID)
 	assert.Nil(t, err)
-
-	// clean up test rules
-	cleanupRules(t)
 }
 
 // Test DeleteRule for 401 Unauthorized operation error
 func TestIntegrationDeleteRuleByIDUnauthorizedOperationError(t *testing.T) {
+	defer cleanupRules(t)
+
 	client := getClient(t)
 	invalidClient := getInvalidClient(t)
 
@@ -475,25 +454,23 @@ func TestIntegrationDeleteRuleByIDUnauthorizedOperationError(t *testing.T) {
 	err = invalidClient.CatalogService.DeleteRule(rule.ID)
 	assert.NotNil(t, err)
 	assert.True(t, strings.Contains(err.Error(), "401 Unauthorized"))
-
-	// clean up test rules
-	cleanupRules(t)
 }
 
 // Test DeleteRule for 404 Rule not found error
 func TestIntegrationDeleteRulebyIDRuleNotFoundError(t *testing.T) {
+	defer cleanupRules(t)
+
 	client := getClient(t)
 
 	err := client.CatalogService.DeleteRule("123")
 	assert.NotNil(t, err)
 	assert.True(t, strings.Contains(err.Error(), "404"))
-
-	// clean up test rules
-	cleanupRules(t)
 }
 
 /*// Currently unable to generate a bad rule
 func TestIntegrationCreateRuleInvalidRuleError(t *testing.T)  {
+	defer cleanupRules(t)
+
 	client := getClient()
 
 	// testing CreateRule for 400 Invalid Rule error
@@ -501,9 +478,6 @@ func TestIntegrationCreateRuleInvalidRuleError(t *testing.T)  {
 	_, err := client.CatalogService.CreateRule(model.Rule{Name: ruleName})
 	assert.NotNil(t, err)
 	assert.True(t, strings.Contains(err.Error(), "400 Invalid"))
-
-	// clean up test rules
-	cleanupRules(t)
 }*/
 
 // todo (Parul): 405 Rule cannot be deleted because of dependencies error case
