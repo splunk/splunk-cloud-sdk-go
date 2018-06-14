21dbdb6a89934edb84c3a2c13ce380531801ce01
Parul Upadhyaya <pupadhyaya@splunk.com>
2018-06-14 13:37:11 -0700

    Added more tests for kvstore index endpoints error scenarios

--- test/playground_integration/kvstore_integration_test.go
+++ test/playground_integration/kvstore_integration_test.go
@@ -2,37 +2,38 @@ package playgroundintegration
 
 import (
 	"github.com/splunk/ssc-client-go/model"
+	"github.com/splunk/ssc-client-go/util"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-var test_collection = "integ_test_collection"
-var test_namespace = "integ_test_namespace"
-var test_index = "integ_testIndex2"
+var testCollection = "integ_test_collection"
+var testNamespace = "integ_test_namespace"
+var testIndex = "integ_test_index"
 
 // Test CreateIndex, GetIndexes and DeleteIndex kvstore endpoints
 func TestIntegrationIndexEndpoints(t *testing.T) {
 	// Create the test collection and test namespace for all the index operations
-	dataset, err := getClient(t).CatalogService.CreateDataset(model.DatasetInfo{Name: test_collection, Kind: "kvcollection", Owner: "integ_test", Module: test_namespace, Capabilities: "1100-11111:00000"})
+	dataset, err := getClient(t).CatalogService.CreateDataset(model.DatasetInfo{Name: testCollection, Kind: "kvcollection", Owner: "integ_test", Module: testNamespace, Capabilities: "1100-11111:00000"})
 
 	// Create Index
 	var fields [1]model.IndexFieldDefinition
 	fields[0] = model.IndexFieldDefinition{Direction: -1, Field: "integ_testField1"}
-	err = getClient(t).KVStoreService.CreateIndex(model.IndexDescription{Name: test_index, Collection: test_collection, Namespace: test_namespace, Fields: fields[:]}, test_namespace, test_collection)
+	err = getClient(t).KVStoreService.CreateIndex(model.IndexDescription{Name: testIndex, Collection: testCollection, Namespace: testNamespace, Fields: fields[:]}, testNamespace, testCollection)
 	assert.Nil(t, err)
 
 	// Validate if the index was created
-	result, err := getClient(t).KVStoreService.GetIndexes(test_namespace, test_collection)
+	result, err := getClient(t).KVStoreService.GetIndexes(testNamespace, testCollection)
 	assert.Nil(t, err)
 	assert.Equal(t, len(result), 1)
-	assert.Equal(t, result[0].Name, test_index)
+	assert.Equal(t, result[0].Name, testIndex)
 
 	// Delete the test index
-	err = getClient(t).KVStoreService.DeleteIndex(test_namespace, test_collection, test_index)
+	err = getClient(t).KVStoreService.DeleteIndex(testNamespace, testCollection, testIndex)
 	assert.Nil(t, err)
 
 	// Validate if the index was deleted
-	result, err = getClient(t).KVStoreService.GetIndexes(test_namespace, test_collection)
+	result, err = getClient(t).KVStoreService.GetIndexes(testNamespace, testCollection)
 	assert.Nil(t, err)
 	assert.Equal(t, len(result), 0)
 
@@ -41,9 +42,45 @@ func TestIntegrationIndexEndpoints(t *testing.T) {
 	assert.Nil(t, err)
 }
 
-// Test CreateIndex, GetIndexes and DeleteIndex kvstore endpoints - Error scenarios
-func TestIntegrationIndexEndpointsErrorScenarios(t *testing.T) {
-	// 422 (If fields null or not present in Index)
-	// 404 (If deleting an index, not present)
-	// 500 Internal server error (If the collection or namespace where the index is being created is not present)
+// Test CreateIndex for 422 Unprocessable Entity error
+func TestIntegrationCreateIndexUnprocessableEntityError(t *testing.T) {
+	// Create the test collection and test namespace
+	dataset, err := getClient(t).CatalogService.CreateDataset(model.DatasetInfo{Name: testCollection, Kind: "kvcollection", Owner: "integ_test", Module: testNamespace, Capabilities: "1100-11111:00000"})
+
+	// Create Index
+	err = getClient(t).KVStoreService.CreateIndex(model.IndexDescription{Name: testIndex, Collection: testCollection, Namespace: testNamespace, Fields: nil}, testNamespace, testCollection)
+	assert.NotNil(t, err)
+	assert.True(t, err.(*util.HTTPError).Status == 422, "Expected error code 422")
+	assert.True(t, err.(*util.HTTPError).Message == "422 Unprocessable Entity", "Expected error message should be 422 Unprocessable Entity")
+
+	// Delete the test collection and test namespace
+	err = getClient(t).CatalogService.DeleteDataset(dataset.ID)
+	assert.Nil(t, err)
+}
+
+// Test CreateIndex for 500 Internal server error
+func TestIntegrationCreateIndexNonExistingCollection(t *testing.T) {
+	// Create Index
+	var fields [1]model.IndexFieldDefinition
+	fields[0] = model.IndexFieldDefinition{Direction: -1, Field: "integ_testField1"}
+	err := getClient(t).KVStoreService.CreateIndex(model.IndexDescription{Name: testIndex, Collection: testCollection, Namespace: testNamespace, Fields: fields[:]}, testNamespace, testCollection)
+	assert.NotNil(t, err)
+	assert.True(t, err.(*util.HTTPError).Status == 500, "Expected error code 500")
+	assert.True(t, err.(*util.HTTPError).Message == "500 Internal Server Error", "Expected error message should be 500 Internal Server Error")
+}
+
+// Test DeleteIndex for 404 Index not found error
+func TestIntegrationDeleteNonExitingIndex(t *testing.T) {
+	// Create the test collection and test namespace
+	dataset, err := getClient(t).CatalogService.CreateDataset(model.DatasetInfo{Name: testCollection, Kind: "kvcollection", Owner: "integ_test", Module: testNamespace, Capabilities: "1100-11111:00000"})
+
+	// DeleteIndex
+	err = getClient(t).KVStoreService.DeleteIndex(testNamespace, testCollection, testIndex)
+	assert.NotNil(t, err)
+	assert.True(t, err.(*util.HTTPError).Status == 404, "Expected error code 404")
+	assert.True(t, err.(*util.HTTPError).Message == "404 Not Found", "Expected error message should be 404 Not Found")
+
+	// Delete the test collection and test namespace
+	err = getClient(t).CatalogService.DeleteDataset(dataset.ID)
+	assert.Nil(t, err)
 }
