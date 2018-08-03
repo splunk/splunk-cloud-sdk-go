82bdd704590a1133dfecdea6f3a73b0780279441
Eric Cheng <xcheng@splunk.com>
2018-08-03 16:42:10 -0700

    change copyright format

--- test/playground_integration/refresh_token_integration_test.go
+++ test/playground_integration/refresh_token_integration_test.go
@@ -1,9 +1,7 @@
-/*
- * Copyright © 2018 Splunk Inc.
- * SPLUNK CONFIDENTIAL – Use or disclosure of this material in whole or in part
- * without a valid written license from Splunk Inc. is PROHIBITED.
- *
- */
+// Copyright © 2018 Splunk Inc.
+// SPLUNK CONFIDENTIAL – Use or disclosure of this material in whole or in part
+// without a valid written license from Splunk Inc. is PROHIBITED.
+//
 
 // +build !integration
 
