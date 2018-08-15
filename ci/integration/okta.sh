4070afbbe186133faf4dfd8ab930d8a023ef861c
Hugbot <loganknecht@users.noreply.github.com>
2018-08-15 10:54:52 -0700

    Remove commented code

--- ci/integration/okta.sh
+++ ci/integration/okta.sh
@@ -1,17 +1,3 @@
-#echo "Fetching Okta bearer token..."
-#
-#export TEST_BEARER_TOKEN=$(curl -s --request POST --url "https://$OKTA_HOST/oauth2/default/v1/token" \
-# --header 'accept: application/json' \
-# --header "Authorization: Basic $OKTA_BASIC" \
-# --header 'content-type: application/x-www-form-urlencoded' --data "$OKTA_BODY" | jq -r ".access_token")
-#
-#if [[ -z "$TEST_BEARER_TOKEN" ]]; then
-#    echo "Unable to set BEARER_TOKEN"
-#    exit 1
-#else
-#    echo "Successfully set BEARER_TOKEN"
-#fi
-#--------------------------------------------------------------------------------
 echo "Fetching Okta bearer token..."
 
 OKTA_AUTHENTICATION_RESPONSE=$(curl --request POST \
