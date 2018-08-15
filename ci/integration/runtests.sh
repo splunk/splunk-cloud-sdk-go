fd43cca27439ff1fd66a6f51f34ad19b0b3e2c13
Hugbot <loganknecht@users.noreply.github.com>
2018-08-15 10:53:20 -0700

    Fix code coverage to correctly output coverage.
    Update the codecov script provided by codecov.io
    Separate the okta token retrieval steps into discrete steps for easier debugging
    Change the testing commands to correctly run code coverage
    Delete runtests scripts that are not used in the codebase

--- ci/integration/runtests.sh
+++ ci/integration/runtests.sh
@@ -8,12 +8,7 @@ echo "==============================================="
 env | grep TEST_SSC_HOST
 env | grep TEST_URL_PROTOCOL
 env | grep TEST_TENANT_ID
-# TODO: Flag output to say if the bearer token was set or not
-#if [ -z ${var+x} ]; then
-#    echo "TEST_BEARER_TOKEN was set.";
-#else
-#    echo "TEST_BEARER_TOKEN was set.";
-#fi
+
 echo "==============================================="
 
 # Get the BEARER_TOKEN setup
@@ -27,16 +22,14 @@ else
 fi
 
 # Required to run just the service tests
-# cd service
-
 if [ "$allow_failures" -eq "1" ]; then
     echo "Running integration tests but not gating on failures..."
     set +e
-    go test -v -covermode=count -coverprofile="codecov.integration.out" ./test/playground_integration/...
+    go test -v -cover -covermode=count -coverprofile="codecov.integration.out" ./test/playground_integration/...
     exit 0
 else
     echo "Running integration tests and gating on failures..."
-    go test -v -covermode=count -coverprofile="codecov.integration.out" ./test/playground_integration/... || exit 1
+    go test -v -cover -covermode=count -coverprofile="codecov.integration.out" ./test/playground_integration/... || exit 1
 fi
 
 # Upload coverage information
