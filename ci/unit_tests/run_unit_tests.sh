5bf38d6e239eb4d59cf328d67de5ee51d6ef21e7
Hugbot <loganknecht@users.noreply.github.com>
2018-08-17 12:00:00 -0700

    Add `run_unit_tests.sh` in order to generate a code cov file for all unit tests.

---
+++ ci/unit_tests/run_unit_tests.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "==============================================="
+echo "Beginning unit tests"
+echo "==============================================="
+
+GO_NON_TEST_NON_VENDOR_PACKAGES=$(go list ./... | grep -v /vendor/ | grep -v test)
+#GO_NON_TEST_NON_VENDOR_PACKAGE_NAMES=$(go list -f "{{.Name}}" ./... | grep -v /vendor/ | grep -v test)
+#echo $GO_NON_TEST_NON_VENDOR_PACKAGE_NAMES
+
+PACKAGE_COVERAGE_PREFIX=./ci/unit_tests/
+PACKAGE_COVERAGE_SUFFIX=_unit_test_code_cov.out
+FULL_UNIT_TEST_CODECOV_FILE_NAME=unit_test_codecov.out
+FULL_UNIT_TEST_CODECOV_PATH=$PACKAGE_COVERAGE_PREFIX$FULL_UNIT_TEST_CODECOV_FILE_NAME
+
+echo "------------------------------------------------------------------------------"
+echo "Unit tests will be output to $FULL_UNIT_TEST_CODECOV_PATH"
+echo "------------------------------------------------------------------------------"
+
+for PACKAGE in $GO_NON_TEST_NON_VENDOR_PACKAGES
+do
+    echo "-------------------------------------------------------------------"
+    echo "Beginning unit tests for $PACKAGE"
+    echo "-------------------------------------------------------------------"
+    SANITIZED_PACKAGE_NAME=$(echo $PACKAGE | sed "s/[\.|\/|-]/_/g")
+    COVERAGE_PACKAGE_OUTPUT_FILE=$PACKAGE_COVERAGE_PREFIX$SANITIZED_PACKAGE_NAME$PACKAGE_COVERAGE_SUFFIX
+
+    # echo $SANITIZED_PACKAGE_NAME
+    # echo $COVERAGE_PACKAGE_OUTPUT_FILE
+
+    go test -v -covermode=count -coverprofile=$COVERAGE_PACKAGE_OUTPUT_FILE $PACKAGE || ERROR="Error testing $PACKAGE"
+
+    if [ -f /tmp/foo.txt ]; then
+        cat $COVERAGE_PACKAGE_OUTPUT_FILE >> $FULL_UNIT_TEST_CODECOV_PATH
+    else
+        echo "No unit test results were found for $PACKAGE"
+    fi
+done
+
+# Upload coverage information with the tag unit
+sh ./ci/codecov -f $FULL_UNIT_TEST_CODECOV_PATH -F unit
