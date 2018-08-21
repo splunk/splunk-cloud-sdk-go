48f0c1bf4849a3b81721072a65e8cc5c02cac104
Tim Pavlik <tpavlik@splunk.com>
2018-08-21 15:42:08 -0700

    Remove unrecommended auth flows from idp.go

--- idp/idp.go
+++ idp/idp.go
@@ -22,10 +22,6 @@ import (
 
 // Supported authentication flows
 //
-//   Authorization code (code) -- not recommended, prefer pkce
-//      client_id + client_secret + redirect_uri + username + password =>
-//          access, id_token, refresh_token[*]
-//
 //   Authorization code with PKCE (pkce) -- known/trusted app
 //      client_id + code challenge + redirect_uri + username + password =>
 //          access, id_token, refresh_token[*]
@@ -34,32 +30,24 @@ import (
 //      client_id + client_secret + custom scope =>
 //          access
 //
-//   Implicit flow (implicit) -- unknown/untrusted app
-//      client_id + redirect_uri + username + password =>
-//          access, id_token
-//
-//   Resource owner password (ropw) -- not recomended, prefer pkce
-//      client_id + client_secret + username + password =>
-//          access, id_token, refresh_token[*]
-//
 //   * refresh_token is available if enabled and if the offline_access scope
 //     is requested.
 //
-// Note: code, pkce and implicit flows are normally browser based and involve
-// redirection. Support is also provided for IdPs with extensions to the
-// standard OIDC flows that allow client code to first authenticate with user
-// credentials against a "primary" endpoint (/authn) and retrieve a one
-// time session token, which when used with these flows, will result in the
-// requested grants being returned directly in the redirect url.
+// Note: pkce is normally browser based and involves redirection. Support is
+// also provided for IdPs with extensions to the standard OIDC flows that allow
+// client code to first authenticate with user credentials against a "primary"
+// endpoint (/authn) and retrieve a one time session token, which when used with
+// these flows, will result in the requested grants being returned directly in
+// the redirect url.
 
 // OIDCScope defines scopes that are OpenID Connect compatible, see: https://openid.net/specs/openid-connect-core-1_0.html#ScopeClaims
 type OIDCScope string
 
 const (
 	// ScopeOpenID - The basic (and required) scope for OpenID Connect
-	ScopeOpenID  OIDCScope = "openid"
+	ScopeOpenID OIDCScope = "openid"
 	// ScopeEmail - This scope value requests access to the email and email_verified Claims
-	ScopeEmail   OIDCScope = "email"
+	ScopeEmail OIDCScope = "email"
 	// ScopeProfile - This scope value requests access to the End-User's default profile Claims, which are: name, family_name,
 	// given_name, middle_name, nickname, preferred_username, profile, picture, website, gender, birthdate, zoneinfo,
 	// locale, and updated_at
@@ -67,7 +55,7 @@ const (
 	// ScopeAddress - This scope value requests access to the address Claim
 	ScopeAddress OIDCScope = "address"
 	// ScopePhone - This scope value requests access to the phone_number and phone_number_verified Claims
-	ScopePhone   OIDCScope = "phone"
+	ScopePhone OIDCScope = "phone"
 )
 
 // Read and decode JSON data from given reader and return as a map.
@@ -289,61 +277,6 @@ func (c *Client) ClientFlow(clientID, clientSecret, scope string) (*Context, err
 	return decode(response)
 }
 
-// CodeFlow will authenticate using an authorization code.
-func (c *Client) CodeFlow(clientID, clientSecret, redirectURI, scope, username, password string) (*Context, error) {
-	// retrieve one-time session token
-	sessionToken, err := c.GetSessionToken(username, password)
-	if err != nil {
-		return nil, err
-	}
-
-	// request authorization code
-	params := url.Values{
-		"client_id":     {clientID},
-		"nonce":         {"none"},
-		"redirect_uri":  {redirectURI},
-		"response_type": {"code"},
-		"scope":         {scope},
-		"sessionToken":  {sessionToken},
-		"state":         {state()}}
-	response, err := get(c.url(c.PathAuthorize), params)
-	if err != nil {
-		return nil, err
-	}
-	if response.StatusCode != http.StatusFound {
-		return nil, httpError(response)
-	}
-
-	// retrieve authorization code from location url query string
-	location := response.Header.Get("Location")
-	locationURL, err := url.Parse(location)
-	if err != nil {
-		return nil, err
-	}
-	code := locationURL.Query().Get("code")
-
-	// exchange authorization code for token(s)
-	data := url.Values{
-		"code":         {code},
-		"grant_type":   {"authorization_code"},
-		"redirect_uri": {redirectURI}}
-	request, err := newFormPost(c.url(c.PathToken), data)
-	if err != nil {
-		return nil, err
-	}
-	request.SetBasicAuth(clientID, clientSecret)
-	response, err = newHTTPClient().Do(request)
-	if err != nil {
-		return nil, err
-	}
-
-	defer response.Body.Close()
-	if response.StatusCode != http.StatusOK {
-		return nil, httpError(response)
-	}
-	return decode(response)
-}
-
 // GetSessionToken Returns a one-time session token by authenticating using a
 // "primary" endpoint (/authn).
 func (c *Client) GetSessionToken(username, password string) (string, error) {
@@ -491,37 +424,6 @@ func decodeFragment(fragment string) (*Context, error) {
 	return context, nil
 }
 
-// ImplicitFlow will authenticate using the "implicit" flow.
-func (c *Client) ImplicitFlow(clientID, redirectURI, scope, username, password string) (*Context, error) {
-	// retrieve one-time session token
-	sessionToken, err := c.GetSessionToken(username, password)
-	if err != nil {
-		return nil, err
-	}
-
-	// request authorization code
-	params := url.Values{
-		"client_id":     {clientID},
-		"nonce":         {"none"},
-		"redirect_uri":  {redirectURI},
-		"response_type": {"token id_token"},
-		"scope":         {scope},
-		"sessionToken":  {sessionToken},
-		"state":         {state()}}
-	response, err := get(c.url(c.PathAuthorize), params)
-	if err != nil {
-		return nil, err
-	}
-
-	// retrieve token(s) from location url fragment
-	location := response.Header.Get("Location")
-	locationURL, err := url.Parse(location)
-	if err != nil {
-		return nil, err
-	}
-	return decodeFragment(locationURL.Fragment)
-}
-
 // Refresh will authenticate using a refresh token.
 func (c *Client) Refresh(clientID, scope, refreshToken string) (*Context, error) {
 	form := url.Values{
@@ -529,21 +431,6 @@ func (c *Client) Refresh(clientID, scope, refreshToken string) (*Context, error)
 		"grant_type":    {"refresh_token"},
 		"refresh_token": {refreshToken},
 		"scope":         {scope}}
-	response, err := formPost(c.url(c.PathToken), form)
-	if err != nil {
-		return nil, err
-	}
-	return decode(response)
-}
-
-// ROPWFlow will authenticate using the "resource owner password" flow.
-func (c *Client) ROPWFlow(clientID, scope, username, password string) (*Context, error) {
-	form := url.Values{
-		"client_id":  {clientID},
-		"grant_type": {"password"},
-		"scope":      {scope},
-		"username":   {username},
-		"password":   {password}}
 	response, err := formPost(c.url(c.PathToken), form)
 	if err != nil {
 		return nil, err
