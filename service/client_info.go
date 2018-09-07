676ace4fb6be9c2fb309758788b5f031c3c323e1
Liying Jiang <ljiang@splunk.com>
2018-09-07 12:04:44 -0700

    revert client-info

--- service/client_info.go
+++ service/client_info.go
@@ -1,7 +1,7 @@
 package service
 
 // Version the released version of the SDK
-const Version = "0.4.0-12-gba94cfa-12-gba94cfa-12-gba94cfa"
+const Version = "0.4.0"
 
 // UserAgent SDK Client Identifier
 const UserAgent = "client-go"
