aa0fd703882b20a608cd34be36045f254dd95316
Eric Cheng <xcheng@splunk.com>
2018-09-10 10:53:47 -0700

    rename files, small updates to models

--- model/search.go
+++ model/search.go
@@ -3,21 +3,21 @@ package model
 // SearchJobBase defines properties allowed (and possibly required) in fully constructed Searchjobs in POST payloads and responses
 type SearchJobBase struct {
 	// The SPL query string.
-	Query            string `json:"query"`
+	Query string `json:"query"`
 	// The module to run the search in.
-	Module           string `json:"module"`
+	Module string `json:"module"`
 	// Should SplunkD produce all fields (including those not explicitly mentioned in the SPL)
-	ExtractAllFields bool   `json:"extractAllFields"`
+	ExtractAllFields bool `json:"extractAllFields"`
 	// The number of seconds to run this search before finalizing.
-	MaxTime          uint   `json:"maxTime,omitempty"`
+	MaxTime uint `json:"maxTime,omitempty"`
 	// Used to convert a formatted time string from {start,end}_time into UTC seconds. The default value is the ISO-8601 format.
-	TimeFormat   string           `json:"timeFormat,omitempty"`
+	TimeFormat string `json:"timeFormat,omitempty"`
 	// The System time at the time the search job was created. Specify a time string to set
 	// the absolute time used for any relative time specifier in the search.
 	// Defaults to the current system time when the Job is created.
-	TimeOfSearch string           `json:"timeOfSearch,omitempty"`
+	TimeOfSearch string `json:"timeOfSearch,omitempty"`
 	// Parameters for the search job, mainly earliest and latest.
-	Parameters   SearchParameters `json:"parameters,omitempty"`
+	Parameters SearchParameters `json:"parameters,omitempty"`
 }
 
 // SearchParameters is the type representing parameters currently earliest & latest on search.
@@ -25,7 +25,7 @@ type SearchParameters struct {
 	// The earliest time in absolute or relative format to retrieve events (only supported if the query supports time-ranges)
 	Earliest string `json:"earliest,omitempty"`
 	// The latest time in absolute or relative format to retrieve events (only supported if the query supports time-ranges)
-	Latest   string `json:"latest,omitempty"`
+	Latest string `json:"latest,omitempty"`
 }
 
 // SearchJobStatus describes status of a search job
@@ -41,39 +41,43 @@ const (
 	Done       SearchJobStatus = "done"
 )
 
-// JobStatus is a single job status common info which is used by CatalogJobStatus and PostJobResponse
-type JobStatus struct {
-	// The id assigned to the search job
-	Id    string `json:"sid"`
-	// The current status of the job
-	Status SearchJobStatus `json:"status"`
-	*SearchJobBase
-	// An estimate of how far through the job is complete
-	PercentComplete  float64 `json:"percentComplete"`
-	// The number of results Splunkd produced so far
-	ResultsAvailable int64   `json:"resultsAvailable"`
-}
-
 // SearchJob represents fully constructed search job including readonly fields.
 type SearchJob struct {
-	*JobStatus
+	*SearchJobBase
+	// The id assigned to the search job
+	Id string `json:"sid"`
+	// The current status of the job
+	Status SearchJobStatus `json:"status"`
+	// An estimate of how far through the job is complete
+	PercentComplete float64 `json:"percentComplete"`
+	// The number of results Splunkd produced so far
+	ResultsAvailable int64 `json:"resultsAvailable"`
 	// Run time messages from Splunkd.
 	Messages SearchJobMessages `json:"messages"`
 }
 
-// PatchJobRequest represents the request payload to the patch jobs endpoint
-type PatchJobRequest struct {
+// PatchJob represents the request payload to the patch jobs endpoint
+type PatchJob struct {
 	Action JobAction `json:"action"`
-	TTL    uint   `json:"ttl"`
 }
 
 // JobAction defines actions to be taken on an existing search job.
 type JobAction string
+
 const (
-	Cancel JobAction = "cancel"
-	Finalize JobAction = "finalize"
-	Touch JobAction = "touch"
-	Save JobAction = "save"
+	CancelJob   JobAction = "cancel"
+	FinalizeJob JobAction = "finalize"
+	TouchJob    JobAction = "touch"
+	SaveJob     JobAction = "save"
+)
+
+type JobMessageType string
+
+const (
+	InfoType  JobMessageType = "INFO"
+	FatalType JobMessageType = "FATAL"
+	ErrorType JobMessageType = "ERROR"
+	DebugType JobMessageType = "DEBUG"
 )
 
 // SearchJobMessages is used in search results or search job.
@@ -92,22 +96,24 @@ type PatchJobResponse struct {
 
 // JobResultsParams specifies the query params when fetching job results
 type JobResultsParams struct {
-	Count      int      `key:"count"`
-	Offset     int      `key:"offset"`
+	Count  int `key:"count"`
+	Offset int `key:"offset"`
 }
 
 // SearchResults represents results from a search job
 type SearchResults struct {
-	Preview     bool                     `json:"preview"`
-	InitOffset  int                      `json:"init_offset"`
+	Preview    bool `json:"preview"`
+	InitOffset int  `json:"init_offset"`
 	// Run time messages from Splunkd.
-	Messages    SearchJobMessages        `json:"messages"`
-	Results     []map[string]interface{} `json:"results"`
-	Fields      []map[string]interface{} `json:"fields"`
+	Messages SearchJobMessages        `json:"messages"`
+	Results  []map[string]interface{} `json:"results"`
+	Fields   []map[string]interface{} `json:"fields"`
 }
 
-// EmptyResultsResponse represents the response when no search results is ready
-type EmptyResultsResponse struct {
+// ResultsNotReadyResponse represents the response when no search results is ready
+type ResultsNotReadyResponse struct {
+	// URL for job results
 	NextLink string `json:"nextLink"`
-	WaitTime string `json:"wait"`
+	// Number of milliseconds to wait before retrying
+	Wait string `json:"wait"`
 }
