1d2e0ca34ac842b74f29acd1d516b6785b609c6e
Shakeel Mohamed <contact@shakeel.xyz>
2018-09-12 15:27:11 -0700

    remove okta.sh (#219)

--- ci/integration/runtests.sh
+++ ci/integration/runtests.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source ./ci/integration/okta.sh
-
 echo "==============================================="
 echo "Beginning integration tests"
 echo "==============================================="
@@ -15,7 +13,7 @@ echo "==============================================="
 CONFIG_FILE="./okta/.token"
 if [ -f $CONFIG_FILE ]; then
     echo "Token found in $CONFIG_FILE"
-    TEST_BEARER_TOKEN=$(cat $CONFIG_FILE)
+    export TEST_BEARER_TOKEN=$(cat $CONFIG_FILE)
 else
     echo "Token was not set to $CONFIG_FILE"
     exit 1
