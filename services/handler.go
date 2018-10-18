e130bed2967199b88f867742252de5ef3401e91c
shilpabijam <36174273+shilpabijam@users.noreply.github.com>
2018-10-18 14:23:34 -0700

    Applat 2879 Retry 429 responses (#269)
    * Spike for retry throttled requests
    
    * add comments in test
    
    * Fix linting errors
    
    * Changed naming, added retry off test
    
    * PR comments and clean up

--- services/handler.go
+++ services/handler.go
@@ -9,11 +9,16 @@ import (
 	"net/http"
 
 	"github.com/splunk/splunk-cloud-sdk-go/idp"
+	"time"
 )
 
 const (
 	// DefaultMaxAuthnAttempts defines the maximum number of retries that will be performed for a request encountering an authentication issue
 	DefaultMaxAuthnAttempts = 1
+)
+
+const (
+	maxRetryCount = 6
 )
 
 // ResponseHandler defines the interface for implementing custom response
@@ -25,6 +30,16 @@ type ResponseHandler interface {
 // AuthnResponseHandler handles logic for updating the client access token in response to 401 errors
 type AuthnResponseHandler struct {
 	TokenRetriever idp.TokenRetriever
+}
+
+// ConfigurableRetryResponseHandler handles logic for retrying requests with user configurable settings for Retry number and interval
+type ConfigurableRetryResponseHandler struct {
+	ConfigurableRetryConfig ConfigurableRetryConfig
+}
+
+// DefaultRetryResponseHandler handles logic for retrying requests with default settings for Retry number and interval
+type DefaultRetryResponseHandler struct {
+	DefaultRetryConfig DefaultRetryConfig
 }
 
 // HandleResponse will retry a request once after re-authenticating if a 401 response code is encountered
@@ -44,5 +59,36 @@ func (rh AuthnResponseHandler) HandleResponse(client *BaseClient, request *Reque
 	// Update the client such that future requests will use the new access token and retain context information
 	client.UpdateTokenContext(ctx)
 	// Retry the request with the updated token
+	return client.Do(request)
+}
+
+// HandleResponse will retry a request once a 429 is encountered using a Default exponential BackOff Retry Strategy
+func (defRh DefaultRetryResponseHandler) HandleResponse(client *BaseClient, request *Request, response *http.Response) (*http.Response, error) {
+	return handleRequestResponse(client, request, response, maxRetryCount, 500)
+}
+
+// HandleResponse will retry a request once a 429 is encountered using a Configurable exponential BackOff Retry Strategy
+func (configRh ConfigurableRetryResponseHandler) HandleResponse(client *BaseClient, request *Request, response *http.Response) (*http.Response, error) {
+	return handleRequestResponse(client, request, response, configRh.ConfigurableRetryConfig.RetryNum, configRh.ConfigurableRetryConfig.Interval)
+}
+
+//handleRequestResponse - helper function to handle the retry to a 429 response
+func handleRequestResponse(client *BaseClient, request *Request, response *http.Response, retryCount uint, interval int) (*http.Response, error){
+	if response.StatusCode != 429 {
+		return response, nil
+	}
+	if request.NumAttempts > retryCount {
+		return response, nil
+	}
+	//implement exponential back off by increasing the waiting time between retries after each retry failure.
+	backoffMillis := time.Duration((1<<request.NumAttempts)*interval) * time.Millisecond
+	time.Sleep(backoffMillis)
+
+	// reinitialize body, otherwise it will be empty
+	body, err := request.GetBody()
+	if err != nil {
+		return nil, err
+	}
+	request.Body = body
 	return client.Do(request)
 }
