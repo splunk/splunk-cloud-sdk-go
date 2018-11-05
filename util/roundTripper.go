a916f04661e2bd28657aefb55349250d74c511df
Eric Cheng <xcheng@splunk.com>
2018-11-05 10:43:38 -0800

    added error handling in roundtripper

--- util/roundTripper.go
+++ util/roundTripper.go
@@ -32,6 +32,9 @@ func (st *SdkTransport) RoundTrip(request *http.Request) (*http.Response, error)
 	st.logger.Print(fmt.Sprintf("===Request:\n%s\n", string(requestDump)))
 
 	response, err := st.transport.RoundTrip(request)
+	if err != nil {
+		return response, err
+	}
 
 	responseDump, err := httputil.DumpResponse(response, true)
 	if err != nil {
