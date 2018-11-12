c6d1c2ac9503c3aab42029bfe56ae84a24db561c
Tim Pavlik <tpavlik@splunk.com>
2018-11-12 12:43:59 -0800

    Escape < and > to &lt; and &gt; per portal requirements.

--- ci/docs/docs_md.sh
+++ ci/docs/docs_md.sh
@@ -18,6 +18,14 @@ for PACKAGE in $GO_NON_TEST_NON_EXAMPLE_PACKAGES
 do
     DOC_FILE=$(echo $PACKAGE | sed "s/[\.|\/|-]/_/g").md
     godocdown $PACKAGE > docs/pkg/$DOC_FILE
+    # Escape < and > to &lt; and &gt; per portal requirements
+    if [ "$(uname)" == "Darwin" ]; then
+        # MacOS
+        sed -E -i '' -e "s/\</\&lt\;/g" -e "s/\>/\&gt\;/g" docs/pkg/$DOC_FILE
+    else
+        # Linux
+        sed -r -i '' -e "s/\</\&lt\;/g" -e "s/\>/\&gt\;/g" docs/pkg/$DOC_FILE
+    fi
     echo "Wrote docs for $PACKAGE to docs/pkg/$DOC_FILE"
     DOC_INDEX+="* [$PACKAGE](pkg/$DOC_FILE)
 "
