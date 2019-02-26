fa146cd62c71078c70b3e89cb9bf5d5ed0888be0
Tim Pavlik <tpavlik@splunk.com>
2019-02-26 14:01:40 -0800

    Remove codecov, we no longer have a subscription

--- ci/integration/runtests.sh
+++ ci/integration/runtests.sh
@@ -42,10 +42,4 @@ else
 fi
 
 # Upload code cov report
-if [[ -z "$CODECOV_TOKEN" ]];
-then
-    echo "THE CODE COVERAGE TOKEN IS NOT SET! CODECOV REPORT WILL NOT BE UPLOADED."
-else
-    # Upload coverage information
-    ./ci/codecov -f $FULL_INTEGRATION_TEST_CODECOV_PATH -F integration -t $CODECOV_TOKEN
-fi
+echo "TODO: CODE COVERAGE IS NOT CURRENTLY SUPPORTED! CODECOV REPORT WILL NOT BE UPLOADED."
