088565f9686763e590c60e02c5c1b4f0335c3b25
Dan Nguyen <dnguyen@splunk.com>
2019-10-22 01:20:23 +0000

    Update client_info.go, version 2.0.2

--- services/client_info.go
+++ services/client_info.go
@@ -2,7 +2,7 @@ package services
 
 // Version the released version of the SDK
 
-const Version = "2.0.1"
+const Version = "2.0.2"
 
 // UserAgent SDK Client Identifier
 const UserAgent = "client-go"
