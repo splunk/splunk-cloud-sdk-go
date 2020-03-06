66230820ae6c31f576ef07b20dc0a3b259102506
Amy Sutedja <asutedja@splunk.com>
2020-03-05 18:11:27 -0800

    Make scloud integration test failures more visible

--- test/scloud/test_appreg.py
+++ test/scloud/test_appreg.py
@@ -23,83 +23,84 @@ class TestAppregistry(unittest.TestCase):
     def test_create_get_delete_app(self):
         appName = "scloudapptesting"
 
-        code, result, _ = appreg("create-app", appName, "web", "--redirect-urls", "https://redirect1.com", "--title",
-                                 "scloudapptitle")
-        self.assertEqual(0, code)
+        code, result, err = appreg("create-app", appName, "web", "--redirect-urls", "https://redirect1.com", "--title",
+                                   "scloudapptitle")
+        self.assertEqual(0, code, err)
         self.assertIsNotNone(result)
 
         # Get-app-Tests
-        code, result, _ = appreg("get-app", appName)
-        self.assertEqual(0, code)
+        code, result, err = appreg("get-app", appName)
+        self.assertEqual(0, code, err)
         self.assertIsNotNone(result)
 
         # List-all-app-Tests
-        code, result, _ = appreg("list-apps")
-        self.assertEqual(0, code)
+        code, result, err = appreg("list-apps")
+        self.assertEqual(0, code, err)
         self.assertIsNotNone(result)
 
         # Update app
-        code, result, _ = appreg("update-app", appName, "--redirect-urls", "https://redirect2.com , https://mycompany.com", "--title",
-                                 "scloudapptitle")
-        self.assertEqual(0, code)
+        code, result, err = appreg("update-app", appName, "--redirect-urls", "https://redirect2.com , https://mycompany.com", "--title",
+                                   "scloudapptitle")
+        self.assertEqual(0, code, err)
         self.assertIsNotNone(result)
 
         # Clean up
-        code, result, _ = appreg("delete-app", appName)
-        self.assertEqual(0, code)
+        code, result, err = appreg("delete-app", appName)
+        self.assertEqual(0, code, err)
 
     def test_create_get_delete_subscription(self):
         appName = "scloudsubscription"
 
         # Create-app
-        code, result, _ = appreg("create-app", appName, "web", "--redirect-urls", "https://redirect1.com", "--title",
-                                 appName)
+        code, result, err = appreg("create-app", appName, "web", "--redirect-urls", "https://redirect1.com", "--title",
+                                   appName)
+        self.assertEqual(0, code, err)
 
         # Create-subscription-Tests
-        code, result, _ = appreg("create-subscription", appName)
-        self.assertEqual(0, code)
+        code, result, err = appreg("create-subscription", appName)
+        self.assertEqual(0, code, err)
 
         # Get-subscription-Tests
-        code, result, _ = appreg("get-subscription", appName)
-        self.assertEqual(0, code)
+        code, result, err = appreg("get-subscription", appName)
+        self.assertEqual(0, code, err)
         self.assertIsNotNone(result)
         # Get-subscription-of Non-exist app Tests
-        code, result, _ = appreg("get-subscription", "nosuchapp")
+        code, result, _err = appreg("get-subscription", "nosuchapp")
         self.assertEqual(1, code)
 
         # List-all-subscriptions-Test
-        code, result, _ = appreg("list-subscriptions", "web")
-        self.assertEqual(0, code)
+        code, result, err = appreg("list-subscriptions", "web")
+        self.assertEqual(0, code, err)
         self.assertIsNotNone(result)
         # Clean up
-        code, result, _ = appreg("delete-subscription", appName)
-        self.assertEqual(0, code)
-        code, result, _ = appreg("delete-app", appName)
-        self.assertEqual(0, code)
+        code, result, err = appreg("delete-subscription", appName)
+        self.assertEqual(0, code, err)
+        code, result, err = appreg("delete-app", appName)
+        self.assertEqual(0, code, err)
 
     def test_rotate_secret(self):
         appName = "scloudrotatesecret"
 
-        code, result, _ = appreg("create-app", appName, "web", "--redirect-urls", "https://redirect1.com", "--title",
-                                 appName)
-        self.assertEqual(0, code)
+        code, result, err = appreg("create-app", appName, "web", "--redirect-urls", "https://redirect1.com", "--title",
+                                   appName)
+        self.assertEqual(0, code, err)
         self.assertIsNotNone(result)
 
         # rotate app secret
-        code, result, _ = appreg("rotate-secret", appName)
-        self.assertEqual(0, code)
+        code, result, err = appreg("rotate-secret", appName)
+        self.assertEqual(0, code, err)
         self.assertIsNotNone(result)
 
         # Clean up
-        code, result, _ = appreg("delete-app", appName)
-        self.assertEqual(0, code)
+        code, result, err = appreg("delete-app", appName)
+        self.assertEqual(0, code, err)
 
     def test_get_spec_json(self):
-        code, result, _ = appreg("get-spec-json")
-        self.assertEqual(0, code)
+        code, result, err = appreg("get-spec-json")
+        self.assertEqual(0, code, err)
         self.assertTrue(result)
 
     def test_get_spec_yaml(self):
-        code, result, _ = appreg("get-spec-yaml")
-        self.assertEqual(0, code)
+        code, result, err = appreg("get-spec-yaml")
+        self.assertEqual(0, code, err)
         self.assertTrue(result)
--- test/scloud/test_forwarders.py
+++ test/scloud/test_forwarders.py
@@ -19,79 +19,77 @@ class TestForwarders(unittest.TestCase):
         self.assertIsNotNone(self.tname)
 
     def tearDown(self):
-        code, result, _ = forwarders("delete-certificates")
-        self.assertEqual(0, code)
+        code, result, err = forwarders("delete-certificates")
+        self.assertEqual(0, code, err)
 
     def test_create_certificates(self):
-        code, result, _ = forwarders("create-certificate", PEM_FILEPATH)
+        code, result, err = forwarders("create-certificate", PEM_FILEPATH)
         print "\n--Create-Certificate-Tests--\n"
         print "Validate Success Code"
         print code
-        self.assertEqual(0, code)
+        self.assertEqual(0, code, err)
         self.assertTrue("content" in result)
 
         print "Validate certificate"
 
-        #Create existing certificate - 400
+        # Create existing certificate - 400
         print "Create existing certificate"
         code, _, err = forwarders("create-certificate", PEM_FILEPATH)
         self.assertTrue(test.is400(err))
 
-        #Clean up
+        # Clean up
         print "Delete certificate"
-        code, result, _ = forwarders("delete-certificates")
-        self.assertEqual(0, code)
+        code, result, err = forwarders("delete-certificates")
+        self.assertEqual(0, code, err)
 
     def test_get_certificates(self):
         print "\n--Get-Certificate-Tests--\n"
         print "Create-Certificate"
-        code, result, _ = forwarders("create-certificate", PEM_FILEPATH)
-        self.assertEqual(0, code)
+        code, result, err = forwarders("create-certificate", PEM_FILEPATH)
+        self.assertEqual(0, code, err)
         self.assertTrue("content" in result)
         print "Get-Certificate"
-        code, result, _ = forwarders("list-certificates")
-        self.assertEqual(0, code)
+        code, result, err = forwarders("list-certificates")
+        self.assertEqual(0, code, err)
 
         print "Delete All certificates"
-        code, result, _ = forwarders("delete-certificates")
-        self.assertEqual(0, code)
+        code, result, err = forwarders("delete-certificates")
+        self.assertEqual(0, code, err)
 
-        #Get certificates when no certificates exist
+        # Get certificates when no certificates exist
         print "Get-Certificate-None-Exists"
-        code, result, _ = forwarders("list-certificates")
+        code, result, err = forwarders("list-certificates")
         print "Validate no certificates"
         self.assertFalse("content" in result)
-        self.assertEqual(0, code)
+        self.assertEqual(0, code, err)
 
     def test_delete_certificates(self):
         print "\n--Delete-Certificates-Tests--\n"
         print "Create-Certificate"
-        code, result, _ = forwarders("create-certificate", PEM_FILEPATH)
-        self.assertEqual(0, code)
+        code, result, err = forwarders("create-certificate", PEM_FILEPATH)
+        self.assertEqual(0, code, err)
         self.assertTrue("content" in result)
 
-        code, result, _ = forwarders("delete-certificates")
-        self.assertEqual(0, code)
+        code, result, err = forwarders("delete-certificates")
+        self.assertEqual(0, code, err)
 
-        code, result, _ = forwarders("list-certificates")
-        self.assertEqual(0, code)
+        code, result, err = forwarders("list-certificates")
+        self.assertEqual(0, code, err)
         self.assertFalse("content" in result)
         print "Validate all certificates are deleted"
 
     def test_get_spec_json(self):
         print "\n--Get-Spec-Json--\n"
-        code, result, _ = forwarders("get-spec-json")
-        self.assertEqual(0, code)
+        code, result, err = forwarders("get-spec-json")
+        self.assertEqual(0, code, err)
         self.assertTrue(result)
 
     def test_get_spec_yaml(self):
         print "\n--Get-Spec-Yml--\n"
-        code, result, _ = forwarders("get-spec-yaml")
-        self.assertEqual(0, code)
+        code, result, err = forwarders("get-spec-yaml")
+        self.assertEqual(0, code, err)
         self.assertTrue(result)
 
 
 if __name__ == "__main__":
     unittest.main()
-
-
