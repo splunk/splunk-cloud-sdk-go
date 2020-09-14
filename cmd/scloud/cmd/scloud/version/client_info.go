390fa98ed0c8ca4840f96d20064f7ca5ca802ff1
Shilpa Bijam <sbijam@splunk.com>
2020-09-14 10:17:08 -0700

    Update client_info.go version to 5.0.0

--- cmd/scloud/cmd/scloud/version/client_info.go
+++ cmd/scloud/cmd/scloud/version/client_info.go
@@ -2,7 +2,7 @@ package version
 
 // Version the released version of the SDK
 
-const ScloudVersion = "4.0.1"
+const ScloudVersion = "5.0.0"
 
 // UserAgent Scloud Client Identifier
 const UserAgent = "scloud"
