9f53626eb641b8fa13c78988c3bbc371a2c9384d
Parul Upadhyaya <@splunk.com>
2020-11-13 10:36:37 -0800

    Updated scloud version for release

--- cmd/scloud/cmd/scloud/version/client_info.go
+++ cmd/scloud/cmd/scloud/version/client_info.go
@@ -2,7 +2,7 @@ package version
 
 // Version the released version of the SDK
 
-const ScloudVersion = "5.0.0"
+const ScloudVersion = "6.0.0"
 
 // UserAgent Scloud Client Identifier
 const UserAgent = "scloud"
