bddf655170d9937865a6d4dbd01cc7475a314bb6
Parul Upadhyaya <@splunk.com>
2021-03-29 21:10:37 -0700

    Update client_info.go version to 7.2.0-beta.1

--- cmd/scloud/cmd/scloud/version/client_info.go
+++ cmd/scloud/cmd/scloud/version/client_info.go
@@ -2,7 +2,7 @@ package version
 
 // Version the released version of the SDK
 
-const ScloudVersion = "7.1.0"
+const ScloudVersion = "7.2.0-beta.1"
 
 // UserAgent Scloud Client Identifier
 const UserAgent = "scloud"
