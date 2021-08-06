23c8ef6f42241c59e66c5f6e30a78e07d2d70d8d
Liying Jiang <ljiang@splunk.com>
2021-08-06 18:39:36 +0000

    Update client_info.go

--- cmd/scloud/cmd/scloud/version/client_info.go
+++ cmd/scloud/cmd/scloud/version/client_info.go
@@ -2,7 +2,7 @@ package version
 
 // Version the released version of the SDK
 
-const ScloudVersion = "8.0.0-beta.4"
+const ScloudVersion = "8.0.0-beta.5"
 
 // UserAgent Scloud Client Identifier
 const UserAgent = "scloud"
