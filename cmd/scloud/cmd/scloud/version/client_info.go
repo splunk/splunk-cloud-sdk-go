ca27668b2db0d31a3b2059d4e3b5e5858c9e14c0
Tim Pavlik <tpavlik@splunk.com>
2021-10-08 13:21:52 -0700

    Bump scloud version

--- cmd/scloud/cmd/scloud/version/client_info.go
+++ cmd/scloud/cmd/scloud/version/client_info.go
@@ -2,7 +2,7 @@ package version
 
 // Version the released version of the SDK
 
-const ScloudVersion = "8.0.0-beta.5"
+const ScloudVersion = "8.0.0-beta.6"
 
 // UserAgent Scloud Client Identifier
 const UserAgent = "scloud"
